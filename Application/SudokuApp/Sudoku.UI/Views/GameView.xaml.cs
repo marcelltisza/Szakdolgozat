c84be17bb6a323fb9cf04197d289859fe95855d6
marcelltisza <tiszamarcell2@gmail.com>
2022-01-13 20:24:03 +0100

    Draw sudoku grid

--- Application/SudokuApp/Sudoku.UI/Views/GameView.xaml.cs
+++ Application/SudokuApp/Sudoku.UI/Views/GameView.xaml.cs
@@ -23,6 +23,51 @@ namespace Sudoku.UI.Views
         public GameView()
         {
             InitializeComponent();
+
+            CreateSudokuGrid();
+        }
+
+        public void CreateSudokuGrid()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                SudokuGrid.RowDefinitions.Add(new RowDefinition());
+                SudokuGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Border border = new Border();
+                    border.BorderBrush = Brushes.Black;
+                    border.BorderThickness = new Thickness(0.5);
+                    Grid.SetRow(border, i);
+                    Grid.SetColumn(border, j);
+                    SudokuGrid.Children.Add(border);
+                    Label label = new Label();
+                    label.Background = Brushes.Transparent;
+                    border.Child = label;
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    var border = new Border
+                    {
+                        BorderThickness = new Thickness(1.5),
+                        BorderBrush = Brushes.Black
+                    };
+                    Grid.SetRow(border, i);
+                    Grid.SetColumn(border, j);
+                    Minigrids.Children.Add(border);
+                    var label = new Label();
+                    label.Background = Brushes.Transparent;
+                    border.Child = label;
+                }
+            }
         }
     }
 }
